7d1a784da96d267ad6ec89ed3332a6295209f7d1
efcaguab <efcaguab@gmail.com>
2015-10-06 15:07:07 +1300

    read maximum matchings in abacus

--- code/06_read_max_match.R
+++ code/06_read_max_match.R
@@ -3,62 +3,59 @@ library(magrittr)
 library(foreach)
 library(doMC)
 registerDoMC(cores = 4)
+i <- as.integer(commandArgs(trailingOnly = T)[1])
+
 # load functions
 a <- list.files("./code/functions", full.names = TRUE) %>% lapply(source)
 
 folder <- "./data/maximum_matchings"
 filenames <- list.files(folder)
 
-plyr::ldply(filenames, function(x){
+space <- plyr::ldply(filenames, function(x){
+	n_lines <- file.path(folder, x) %>% paste("wc -l", .) %>%
+		system(intern = T) %>%
+		stringr::str_split(" ") %>% 
+		unlist() %>%
+		magrittr::extract(1) %>%
+		as.numeric()
 	
- n_lines <- file.path(folder, x) %>% paste("wc -l", .) %>%
- 	system(intern = T) %>%
- 	stringr::str_split(" ") %>% 
- 	unlist() %>%
- 	magrittr::extract(1) %>%
- 	as.numeric()
- 
- m_size <- scan(file.path(folder, x), nlines = 1) %>% length()
 	
- net <- x %>% stringr::str_split("_") %>% unlist() %>% extract(1)
- type <- x %>% stringr::str_split("_") %>% unlist() %>% extract(2)
- keep <- x %>% stringr::str_split("_") %>% unlist() %>% extract(3) %>%
- 	stringr::str_split("\\.") %>% unlist() %>% extract(1)
- net <- "./data/networks" %>% file.path(paste0(net, ".dat")) %>%
- 	readRDS() %>%
- 	# select only the largest connected component of the network
- 	keep_largest_component()
- net_bi <- net %>%
- 	# transform the network into the bipartite format we need
- 	bipartite_digraph(type = type, keep = keep) %>%
- 	digraph_bipartite() %>%
- 	igraph::add_layout_(igraph::as_bipartite())
- 
- 
- 
- freq <- seq(1, n_lines, by = 1000) %>%
- 	plyr::adply(1, function(y) {
- 		 f <- scan(file.path(folder, x), nlines = 1000, skip = y-1) %>% 
- 			matrix(ncol = m_size, byrow = T) %>%
- 			apply(1, function(z){
- 				matched <- igraph::ends(net_bi, igraph::E(net_bi)[z + 1])[, 2]
- 				!(igraph::V(net_bi)[igraph::V(net_bi)$type]$name %in% matched)
- 			}) %>%
- 			apply(1, sum)
- 		 
- 		 dplyr::data_frame(node = igraph::V(net_bi)[igraph::V(net_bi)$type]$name,
- 		 									freq = f) %>%
- 		 	dplyr::rowwise() %>%
- 		 	dplyr::mutate(node = stringr::str_split(node, "\\.") %>% 
- 		 									unlist() %>% magrittr::extract(1))
- 	}, .parallel = T)
- 
+	data.frame(file = x, line = seq(1, n_lines, by = 100000)) 
+}) 
+p <- space[i, ]
 
- freq %>%
- 	dplyr::group_by(node) %>%
- 	dplyr::summarise(freq = sum (freq)) %>% 
- 	dplyr::mutate(name = unlist(stringr::str_split(x, "_"))[1],
- 								type = unlist(stringr::str_split(x, "_"))[2]) 
+m_size <- scan(file.path(folder, p$file), nlines = 1) %>% length()
 
-}, .progress = "text") %>%
-	write.csv(file = "./data/node_max-matching.dat", row.names = F)
+net <- p$file %>% stringr::str_split("_") %>% unlist() %>% extract(1)
+type <- p$file %>% stringr::str_split("_") %>% unlist() %>% extract(2)
+keep <- p$file %>% stringr::str_split("_") %>% unlist() %>% extract(3) %>%
+	stringr::str_split("\\.") %>% unlist() %>% extract(1)
+
+net <- "./data/networks" %>% file.path(paste0(net, ".dat")) %>%
+	readRDS() %>%
+	# select only the largest connected component of the network
+	keep_largest_component()
+net_bi <- net %>%
+	# transform the network into the bipartite format we need
+	bipartite_digraph(type = type, keep = keep) %>%
+	digraph_bipartite() %>%
+	igraph::add_layout_(igraph::as_bipartite())
+
+f <- scan(file.path(folder, p$file), nlines = 100000, skip = p$line-1) %>% 
+	matrix(ncol = m_size, byrow = T) %>%
+	apply(1, function(z){
+		matched <- igraph::ends(net_bi, igraph::E(net_bi)[z + 1])[, 2]
+		!(igraph::V(net_bi)[igraph::V(net_bi)$type]$name %in% matched)
+	}) %>%
+	apply(1, sum)
+
+dplyr::data_frame(node = igraph::V(net_bi)[igraph::V(net_bi)$type]$name,
+									freq = f) %>%
+	dplyr::rowwise() %>%
+	dplyr::mutate(node = stringr::str_split(node, "\\.") %>% 
+									unlist() %>% magrittr::extract(1))
+
+filename <- p$file %>% stringr::str_split("\\.") %>% unlist() %>% magrittr::extract(1) %>%
+	paste(p$line, sep = "_")
+
+write.csv(file = file.path("./data/maximum_matchings_summary", filename), row.names = F)
---
+++ code/_06_read_max_match.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#$ -V
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -t 1-1108
+#$ -m a
+#$ -M efc29@uclive.ac.nz
+#$ -r yes
+
+Rscript --no-save --no-restore 06_read_max_match.R $SGE_TASK_ID 
+exit 0
