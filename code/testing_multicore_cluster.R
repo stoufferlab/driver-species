34361b0b6e6e7b15ef0422a4ac8c32e28988e97f
efcaguab <efcaguab@gmail.com>
2016-12-01 16:32:48 +1300

    Remove testing scripts

--- code/testing_multicore_cluster.R
+++
@@ -1,9 +0,0 @@
-library(doMC)
-registerDoMC(cores = 8L)
-
-
-plyr::l_ply(1:8, function(x) {
-	Sys.sleep(40)
-	name <- paste("core", x, "; task", as.numeric(commandArgs(trailingOnly = T)[1]))
-	writeLines(name, paste0("~/core", x, "_task", as.numeric(commandArgs(trailingOnly = T)[1]), ".txt"))
-}, .parallel = TRUE)
