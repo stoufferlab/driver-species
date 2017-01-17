210f4e2b1382143279cc8dc885dca4718c7f5893
Fernando Cagua <efcaguab@gmail.com>
2017-01-17 16:40:38 +1300

    Make bipartite rep. nodes consistent with directed rep. nodes

--- code/functions/digraph_bipartite.R
+++ code/functions/digraph_bipartite.R
@@ -8,7 +8,7 @@
 #' @export
 #'
 #' @examples
-digraph_bipartite <- function(net){
+digraph_bipartite <- function(net, type = "weight"){
 	edges <- dplyr::data_frame(di_name = igraph::V(net)$name,
 														 name = paste(di_name, "fr", sep = "."),
 														 guild = igraph::V(net)$type,
@@ -18,8 +18,15 @@ digraph_bipartite <- function(net){
 														guild = igraph::V(net)$type,
 														type = "to"))
 	adj_matrix <- igraph::as_adjacency_matrix(net, sparse = F, attr = "weight")
-	rownames(adj_matrix) <- dplyr::filter(edges, type == "from") %$% name
-	colnames(adj_matrix) <- dplyr::filter(edges, type == "to") %$% name
+	
+	if(type == "z-bi"){
+		adj_matrix %<>% t()
+		colnames(adj_matrix) <- dplyr::filter(edges, type == "from") %$% name
+		rownames(adj_matrix) <- dplyr::filter(edges, type == "to") %$% name
+	} else {
+		rownames(adj_matrix) <- dplyr::filter(edges, type == "from") %$% name
+		colnames(adj_matrix) <- dplyr::filter(edges, type == "to") %$% name
+	}
 	igraph::graph_from_incidence_matrix(adj_matrix, weight = TRUE, 
 																					 directed = F, mode = "out") %>% 
 		igraph::set_vertex_attr(name = "guild", value = edges$guild) %>%
