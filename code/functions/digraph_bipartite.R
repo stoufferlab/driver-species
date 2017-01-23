029660edb50c2ad425904b8029f53b3475b0aaf0
Fernando Cagua <efcaguab@gmail.com>
2017-01-23 16:14:27 +1300

    Improve documentation

--- code/functions/digraph_bipartite.R
+++ code/functions/digraph_bipartite.R
@@ -1,12 +1,17 @@
-#' Title
-#'
-#' takes a directed graph and convert it into a bipartite representation with duplicated nodes
-#'
-#' @param net 
-#'
-#' @return
+#' 
+#' 
+#' takes a directed graph and convert it into a bipartite representation with
+#' duplicated nodes
+#' 
+#' Before using the function for production, ensure that the edge order is the
+#' same in the input and the output network. That will make things easier when
+#' mapping back the matchings to the original directed network
+#' 
+#' @param net an igraph object
+#' @param type ignore for unweighted networks
+#' @return the bipartite representation as an igraph object
 #' @export
-#'
+#' 
 #' @examples
 digraph_bipartite <- function(net, type = "weight"){
 	edges <- dplyr::data_frame(di_name = igraph::V(net)$name,
