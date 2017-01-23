5b7d1ed97ea59140a3fdb84fd6e38310547441fa
Fernando Cagua <efcaguab@gmail.com>
2017-01-23 16:35:24 +1300

    use 32 cores instead of 48

--- code/_09-calculate_matchings_weighted.sh
+++ code/_09-calculate_matchings_weighted.sh
@@ -4,11 +4,11 @@
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
-#$ -pe multi_thread 48
+#$ -pe multi_thread 32
 #$ -m a
 #$ -M efc29@uclive.ac.nz
 #$ -r yes
 #$ -l mem_free=16G,h_vmem=16G
 
-Rscript --no-save --no-restore 09-calculate_matchings_weighted.R 48 
+Rscript --no-save --no-restore 09-calculate_matchings_weighted.R 32
 exit 0
