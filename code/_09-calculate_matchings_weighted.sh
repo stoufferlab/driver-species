e4f4ad1f93c75ab1e978166373991673ea973c12
efcaguab <efcaguab@gmail.com>
2017-01-24 16:07:36 +1300

    change so that number of cores is specified inline

--- code/_09-calculate_matchings_weighted.sh
+++ code/_09-calculate_matchings_weighted.sh
@@ -4,11 +4,11 @@
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
-#$ -pe multi_thread 32
+##$ -pe multi_thread ${n_cores} # needs to be specified externally
 #$ -m a
 #$ -M efc29@uclive.ac.nz
 #$ -r yes
 #$ -l mem_free=16G,h_vmem=16G
 
-Rscript --no-save --no-restore 09-calculate_matchings_weighted.R 32 $1
+Rscript --no-save --no-restore 09-calculate_matchings_weighted.R $2 $1
 exit 0
