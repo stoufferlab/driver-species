75f8a8a1c051c13fbb54b1acd572a0dfa007475c
Fernando Cagua <efcaguab@gmail.com>
2018-05-14 18:12:44 +1200

    Find control capacity

--- code/matchings.R
+++ code/matchings.R
@@ -58,13 +58,16 @@ maximum_matching <- function(x, weighted = FALSE){
    
    x %<>%
      igraph::set_graph_attr("matching_size", m$matching_size) %>%
-     igraph::set_graph_attr("bipartite_representation", x_bip) %>%
-     `class<-`(c(class(.), "matched_graph"))
+     igraph::set_graph_attr("bipartite_representation", x_bip)
    
    return(x)
 }
 
 get_input_graph <- function(x){
+  
+  if(!"matching_size" %in% graph_attr_names(x)){
+    x %<>% maximum_matching()
+  }
   
   driver <- igraph::V(x)[!igraph::V(x)$matched]
   non_driver <- igraph::V(x)[igraph::V(x)$matched]
@@ -74,17 +77,14 @@ get_input_graph <- function(x){
     purrr::map(make_graph_from_vertex) %>%
     do.call(union_input_graphs, .)
   
-  input_graph(driver) %>%
-    set_vertex_attr("input_node", value = TRUE)
-  
+ 
   ig <- input_graph(driver) %>%
-    set_vertex_attr("input_node", value = TRUE) %>%
+    igraph::set_vertex_attr("input_node", value = TRUE) %>%
     union_input_graphs(input_graph(non_driver) %>% 
                          set_vertex_attr("input_node", value = F), 
                        delete_graph_attr = F) 
   
   set_graph_attr(x, "input_graph", ig)
-  x
 }
 
 get_control_adjacent <- function(x, d){
@@ -128,7 +128,7 @@ union_input_graphs <- function(..., delete_graph_attr = TRUE){
       purrr::map(~ coalece_attribute(joint_network, at[i], .)) %>%
       do.call(dplyr::coalesce, .)
     for(j in 1:n_nets){
-      if(include_graph_attr){
+      if(delete_graph_attr){
         joint_network %<>%
           igraph::delete_graph_attr(paste("name", j, sep = "_")) %>%
           igraph::delete_graph_attr(paste("loops", j, sep = "_"))
@@ -144,4 +144,23 @@ union_input_graphs <- function(..., delete_graph_attr = TRUE){
 
 coalece_attribute <- function(x, y, n){
   vertex_attr(x, name = paste(y, n, sep = "_"))
+}
+
+control_capacity <- function(x){
+  
+  if(!"input_graph" %in% graph_attr_names(x)){
+    x %<>% get_input_graph()
+  }
+  
+  comp <- induced_subgraph(x$input_graph, V(x$input_graph)[V(x$input_graph)$input_node]) %>%
+    components() 
+  
+  n_control_configurations <- prod(comp$csize)
+  cc <- 1 / comp$csize
+
+  V(x$input_graph)$control_capacity <- 0
+  V(x$input_graph)[V(x$input_graph)$input_node]$control_capacity <- rep(cc, rle(comp$membership)$lengths)
+  
+  V(x)[V(x$input_graph)$name]$control_capacity <- V(x$input_graph)$control_capacity
+  x
 }
