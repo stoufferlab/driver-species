915cfda0d10da81e5cecb942a3c8b4158f22a7c2
Fernando Cagua <efcaguab@gmail.com>
2018-05-14 17:25:57 +1200

    function to calculate maximum matching of directed graph

--- code/matchings.R
+++ code/matchings.R
@@ -24,3 +24,43 @@ alternative_bipartite <- function(x, weighted = FALSE){
     igraph::graph_from_incidence_matrix(weighted = attribute) %>%
     igraph::set_vertex_attr("legacy.type", value = igraph::V(x)$type)
 }
+
+maximum_matching <- function(x, weighted = FALSE){
+  
+  x_bip <- alternative_bipartite(x, weighted = weighted)
+  
+  m <- igraph::max_bipartite_match(x_bip)
+  
+  igraph::V(x)$matched <- m %$%
+    matching %>%
+    extract(igraph::V(x_bip)$type) %>%
+    is.na(.) %>%
+    not()
+  
+   matched_edges_bip <- m %$%
+    matching %>%
+    extract(igraph::V(x_bip)$type) %>% {
+      cbind(from = ., 
+            to = names(.))
+    } %>% 
+     dplyr::as_data_frame() %>%
+     dplyr::filter(!is.na(from)) %>%
+     as.matrix() %>%
+     t()
+   
+   igraph::E(x_bip)$matched <- FALSE
+   igraph::E(x_bip)[igraph::get.edge.ids(x_bip, matched_edges_bip, error = F)]$matched <- TRUE
+   
+   matched_edges <- matched_edges_bip %>% stringi::stri_sub(., 1, -4)
+   
+   igraph::E(x)$matched <- FALSE
+   igraph::E(x)[igraph::get.edge.ids(x, matched_edges, error = F)]$matched <- TRUE
+   
+   x %<>%
+     igraph::set_graph_attr("matching_size", m$matching_size) %>%
+     igraph::set_graph_attr("bipartite_representation", x_bip) %>%
+     `class<-`(c(class(.), "matched_graph"))
+   
+   return(x)
+}
+
