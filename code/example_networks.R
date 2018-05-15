d5d507ba354cc43d56037e1fa95793a81fc2f791
Fernando Cagua <efcaguab@gmail.com>
2018-05-15 14:39:30 +1200

    Change type for control_type attribute for consistency

--- code/example_networks.R
+++ code/example_networks.R
@@ -2,25 +2,25 @@ example_ntw_structural <- function(){
   en_controllable <- list(
     edges = dplyr::data_frame(from = c("x_1", "x_2", "u_1"), 
                               to   = c("x_2", "x_3", "x_1"),
-                              type = c("a", "a", "b")), 
+                              control_type = c("a", "a", "b")), 
     vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "u_1"), 
-                               type = c("a", "a", "a", "b"))
+                               control_type = c("a", "a", "a", "b"))
   )
   
   en_inaccessible <- list(
     edges = dplyr::data_frame(from = c("x_1", "x_3", "u_1"), 
                               to   = c("x_2", "x_2", "x_1"),
-                              type = c("a", "a", "b")), 
+                              control_type = c("a", "a", "b")), 
     vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "u_1"),
-                               type = c("a", "a", "a", "b"))
+                               control_type = c("a", "a", "a", "b"))
   )
   
   en_dilation <- list(
     edges = dplyr::data_frame(from = c("x_2", "x_2", "u_1"), 
                               to   = c("x_1", "x_3", "x_2"),
-                              type = c("a", "a", "b")),
+                              control_type = c("a", "a", "b")),
     vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "u_1"), 
-                               type = c("a", "a", "a", "b"))
+                               control_type = c("a", "a", "a", "b"))
   )
   
   en_inaccessible_ok <- add_vertex_edge(en_inaccessible, "u_2", "b", "u_2", "x_3", "b")
