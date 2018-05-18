91e2a9c79a6ce757b804d761657ca0a718bd2504
Fernando Cagua <efcaguab@gmail.com>
2018-05-18 17:52:08 +1200

    Minor changes to example networks

--- code/example_networks.R
+++ code/example_networks.R
@@ -36,18 +36,20 @@ example_ntw_directions <- function(){
   en_vis <- list(
     edges = dplyr::data_frame(from = c("p_1", "p_2", "p_2", "p_3", "p_3"), 
                               to = c("a_1", "a_1", "a_2", "a_2", "a_1"), 
-                              weight = c(1, 2, 5, 1, 1)), 
+                              weight = c(1, 2, 3, 1, 1)), 
     vertex = dplyr::data_frame(name = c("p_1", "p_2", "p_3", "a_1", "a_2"), 
                                type = c("p", "p", "p", "a", "a"))
   )
   
   en_bip <- igraph::graph_from_data_frame(en_vis$edges, directed = F, vertices = en_vis$vertex) 
   en_dir <- as_directed_network(en_bip, direction = "asymmetry", ties = "both", higher_level = "a")
+  # plot(en_dir)
+  # length(all_matchings(en_dir))
   
   list(en_bip, en_dir)
 }
 
-example_matchings_chain <- function(){
+example_ntw_matchings_chain <- function(){
   list(
     edges = dplyr::data_frame(from = c("x_1", "x_2", "x_3"), 
                               to =   c("x_2", "x_3", "x_4"), 
@@ -59,11 +61,17 @@ example_matchings_chain <- function(){
 }
 
 
-example_matchings_star <- function(){
+example_ntw_matchings_star <- function(){
   list(
-    edges = dplyr::data_frame(from = c("x_1", "x_1", "x_1", "x_1"), 
-                              to   = c("x_2", "x_3", "x_4", "x_5")), 
-    vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "x_4", "x_5"))
+    edges = dplyr::data_frame(from = c("x_1", "x_1", "x_1"), 
+                              to   = c("x_2", "x_3", "x_4" )), 
+    vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "x_4"))
   ) %$%
     igraph::graph_from_data_frame(edges, directed = T, vertices = vertex) 
+}
+
+matchings_example_networks <- function(x){
+  x %>%
+    all_matchings() %>%
+    purrr::map(add_control_nodes)
 }
