3c850f94ec93a4ef0105b90d20510cb90349db33
Fernando Cagua <efcaguab@gmail.com>
2018-06-08 13:43:18 +1200

    Create ggplot theme for the ms

---
+++ code/ggplot_theme.R
@@ -0,0 +1,13 @@
+base_ggplot_theme <- function(){
+  theme_bw() +
+    theme(text = element_text(family = "Helvetica"),
+          title = element_text(size = 7, hjust = 0),
+          legend.title = element_text(size = 8),
+          legend.text = element_text(size = 7),
+          axis.text = element_text(size = 7),
+          axis.title = element_text(size = 8, hjust = 0.5), 
+          strip.text = element_text(size = 8, hjust = 0), 
+          strip.background = element_blank(),
+          plot.margin = grid::unit(c(5, 0.5, 2, 0), "mm"),
+          panel.grid = element_blank())
+}
