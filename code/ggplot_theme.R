05e26e1dd909b1acd370c6d3643ff2e456402c2a
Fernando Cagua <efcaguab@gmail.com>
2018-06-09 21:27:59 +1200

    Reduce margings of ggplot theme

--- code/ggplot_theme.R
+++ code/ggplot_theme.R
@@ -8,6 +8,6 @@ base_ggplot_theme <- function(){
           axis.title = element_text(size = 8, hjust = 0.5), 
           strip.text = element_text(size = 8, hjust = 0), 
           strip.background = element_blank(),
-          plot.margin = grid::unit(c(5, 0.5, 2, 0), "mm"),
+          plot.margin = grid::unit(c(0.5, 0.5, 0.5, 0.5), "mm"),
           panel.grid = element_blank())
 }
