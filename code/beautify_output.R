befe4b9b98479eb2c2a315f1df189b4b93fa2ea0
Fernando Cagua <efcaguab@gmail.com>
2018-06-17 16:37:53 +1200

    Function to beautify model term names

---
+++ code/beautify_output.R
@@ -0,0 +1,15 @@
+pretty_term_names <- function(x){
+  x %>%
+    stringr::str_replace(stringr::fixed("species.strength"), 
+                         "visitation strength")%>%
+    stringr::str_replace(stringr::fixed("nestedcontribution"), 
+                         "contribution to nestedness")%>%
+    stringr::str_replace(stringr::fixed("sd_(Intercept).guild"), 
+                         "std. dev. (guild)")%>%
+    stringr::str_replace(stringr::fixed("sd_interaction.push.pull.guild"), 
+                         "std. dev. (asymmetry | guild)")%>%
+    stringr::str_replace(stringr::fixed("cor_(Intercept).interaction.push.pull.guild"), 
+                         "cor. (asymmetry | guild)") %>%
+    stringr::str_replace(stringr::fixed("interaction.push.pull"), 
+                         "asymmetry")
+}
