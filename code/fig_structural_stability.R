67355751e236f5acef6cdacc5d1c0f914adb91de
Fernando Cagua <efcaguab@gmail.com>
2018-06-17 16:35:24 +1200

    Figure structural stability shows density of critical and redundant spp

--- code/fig_structural_stability.R
+++ code/fig_structural_stability.R
@@ -1,34 +1,55 @@
 make_fig_structural_stability <- function(sl_characteristics, metadata){
   require(ggplot2)
-  p1 <- sl_characteristics %>%
+
+  st_df <- sl_characteristics %>%
     filter_networks_df(metadata) %>%
     dplyr::inner_join(metadata, by = "net_name") %>%
     dplyr::group_by(net_name) %>%
-    dplyr::filter(is.finite(feasibility), 
-                  guild == "pla") %>%
-    dplyr::mutate(feasibility1 = dplyr::percent_rank(feasibility),
-                  feasibility2 = scale(feasibility), 
-                  control_capacity = control_capacity) %>%
-    ggplot(aes(x = feasibility2 , y = control_capacity)) +
-    # scale_y_log10() +
-    # geom_point() +
-    geom_smooth(aes(group = net_name), 
-                method = "glm", 
-                se = F, 
-                size =0.25, 
-                method.args = list(family = "binomial"), 
-                colour = "grey", 
-                alpha = 0.25) +
-    geom_smooth(method = "glm", 
-                se = F, 
-                size =0.5, 
-                method.args = list(family = "binomial"), 
-                colour = "black") +
+    dplyr::filter(is.finite(feasibility)) %>%
+    dplyr::mutate(feasibility = scale(feasibility), 
+                  critical = dplyr::case_when(
+                    control_capacity == 1 ~ "critical", 
+                    # control_capacity == 0 ~ "redundant",
+                    TRUE ~ "redundant"
+                  ), 
+                  value = feasibility) %>%
+    dplyr::filter(!is.na(critical)) 
+    
+  means <- st_df %>%
+    dplyr::group_by(critical) %>%
+    dplyr::summarise_if(is.numeric, mean, na.rm = T)
+  
+  value <- 1
+  my.labs <- list(bquote(paste("critical species (", phi==.(value), ")")), 
+                  bquote(paste("redundant (", phi<.(value), ")")))
+  
+  p1 <- st_df %>%
+    ggplot(aes(x = value, colour = critical)) +
+    stat_density(geom = "line", position = "identity", 
+                 size = 0.5) +
+    geom_vline(data = means, 
+               aes(xintercept = value, colour = critical), 
+               linetype = 2, 
+               size = 0.25, 
+               show.legend = F) +
+    scale_color_manual(values = c(my_pallete()$dark_orange, 
+                                  my_pallete()$light_orange), 
+                       name = "", 
+                       labels = my.labs) + 
     base_ggplot_theme() +
-    labs(title = "structural stability of species removal", 
-         x = latex2exp::TeX("scaled feasibility ($\\Omega$)"), 
-         y = latex2exp::TeX("control capacity ($\\phi$)"))
+    theme(legend.position = c(0, 1.15),
+          legend.justification = c(0,1),
+          legend.background = element_rect(fill = "NA"), 
+          legend.key.size = unit(0.15, "in")) +
+    labs(title = "structural stability of species removal",
+         x = latex2exp::TeX("scaled feasibility ($\\Omega$)"))
+  
+  st_df  %$%
+    t.test(feasibility ~ critical, alternative = "less", conf.int = T)
   
   list(p1)
 
 }
+
+
+
