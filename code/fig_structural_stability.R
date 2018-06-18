b76755159137be2b70c0db42b957bc7d0f8648af
Fernando Cagua <efcaguab@gmail.com>
2018-06-19 10:30:15 +1200

    Move code that transforms the df for the structural stability figure

--- code/fig_structural_stability.R
+++ code/fig_structural_stability.R
@@ -1,20 +1,8 @@
-make_fig_structural_stability <- function(sl_characteristics, metadata){
+make_fig_structural_stability <- function(critical_sp_df){
   require(ggplot2)
 
-  st_df <- sl_characteristics %>%
-    filter_networks_df(metadata) %>%
-    dplyr::inner_join(metadata, by = "net_name") %>%
-    dplyr::group_by(net_name) %>%
-    dplyr::filter(is.finite(feasibility)) %>%
-    dplyr::mutate(feasibility = scale(feasibility), 
-                  critical = dplyr::case_when(
-                    control_capacity == 1 ~ "critical", 
-                    # control_capacity == 0 ~ "redundant",
-                    TRUE ~ "redundant"
-                  ), 
-                  value = feasibility) %>%
-    dplyr::filter(!is.na(critical)) 
-    
+  st_df <- critical_sp_df
+  
   means <- st_df %>%
     dplyr::group_by(critical) %>%
     dplyr::summarise_if(is.numeric, mean, na.rm = T)
@@ -42,14 +30,26 @@ make_fig_structural_stability <- function(sl_characteristics, metadata){
           legend.background = element_rect(fill = "NA"), 
           legend.key.size = unit(0.15, "in")) +
     labs(title = "structural stability of species removal",
-         x = latex2exp::TeX("scaled feasibility ($\\Omega$)"))
-  
-  st_df  %$%
-    t.test(feasibility ~ critical, alternative = "less", conf.int = T)
+         x = latex2exp::TeX("scaled structural stability ($\\Omega$)"))
   
   list(p1)
 
 }
 
+extract_critical_sp_df <- function(sl_characteristics, metadata){
+  sl_characteristics %>%
+    filter_networks_df(metadata) %>%
+    dplyr::inner_join(metadata, by = "net_name") %>%
+    dplyr::group_by(net_name) %>%
+    dplyr::filter(is.finite(feasibility)) %>%
+    dplyr::mutate(feasibility = scale(feasibility), 
+                  critical = dplyr::case_when(
+                    control_capacity == 1 ~ "critical", 
+                    # control_capacity == 0 ~ "redundant",
+                    TRUE ~ "redundant"
+                  ), 
+                  value = feasibility) %>%
+    dplyr::filter(!is.na(critical)) 
+}
 
 
