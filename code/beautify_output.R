3792f1d6796deb9197a344302fb633de24af2b1f
Fernando Cagua <efcaguab@gmail.com>
2018-06-26 18:18:06 +1200

    Function that takes term names needs to transform into short names

--- code/beautify_output.R
+++ code/beautify_output.R
@@ -1,17 +1,41 @@
-pretty_term_names <- function(x){
-  x %>%
-    stringr::str_replace(stringr::fixed("species.strength"), 
-                         "visitation strength")%>%
-    stringr::str_replace(stringr::fixed("nestedcontribution"), 
-                         "contribution to nestedness")%>%
-    stringr::str_replace(stringr::fixed("sd_(Intercept).guild"), 
-                         "std. dev. (guild)")%>%
-    stringr::str_replace(stringr::fixed("sd_interaction.push.pull.guild"), 
-                         "std. dev. (asymmetry | guild)")%>%
-    stringr::str_replace(stringr::fixed("cor_(Intercept).interaction.push.pull.guild"), 
-                         "cor. (asymmetry | guild)") %>%
-    stringr::str_replace(stringr::fixed("interaction.push.pull"), 
-                         "asymmetry")
+pretty_term_names <- function(x, version = c("long", "short")){
+  if(version[1] == "long"){
+    x %<>%
+      stringr::str_replace(stringr::fixed("species.strength"), 
+                           "visitation strength")%>%
+      stringr::str_replace(stringr::fixed("nestedcontribution"), 
+                           "contribution to nestedness")%>%
+      stringr::str_replace(stringr::fixed("sd_(Intercept).guild"), 
+                           "std. dev. (guild)")%>%
+      stringr::str_replace(stringr::fixed("sd_interaction.push.pull.guild"), 
+                           "std. dev. (asymmetry | guild)")%>%
+      stringr::str_replace(stringr::fixed("cor_(Intercept).interaction.push.pull.guild"), 
+                           "cor. (asymmetry | guild)") %>%
+      stringr::str_replace(stringr::fixed("interaction.push.pull"), 
+                           "asymmetry") %>%
+      stringr::str_replace(stringr::fixed("guildpol"), 
+                           "level (pol.)")
+  } else if (version[1] == "short"){
+    x %<>%
+      stringr::str_replace(stringr::fixed("species.strength"), 
+                           "$s$") %>%
+      stringr::str_replace(stringr::fixed("nestedcontribution"), 
+                           "$n$") %>%
+           stringr::str_replace(stringr::fixed("interaction.push.pull"), 
+                           "$a$") %>%
+      stringr::str_replace(stringr::fixed("(Intercept)"), 
+                           "int.") %>%
+      stringr::str_replace(stringr::fixed("degree"), 
+                           "$k$") %>%
+      stringr::str_replace(stringr::fixed("guild"), 
+                           "$l$") %>%
+      stringr::str_replace(stringr::fixed("df"), 
+                           "d.f.") %>%
+      stringr::str_replace(stringr::fixed("delta"), 
+                           "$\\Delta$AICc")
+      
+  }
+  x
 }
 
 pretty_net_names <- function(x){
